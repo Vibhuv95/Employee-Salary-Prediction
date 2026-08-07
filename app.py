19394cb5d9333aa9b68734acb58e22bc14b503ce
vIbHugAUtaMcOdE <vibhugautam8081371284@gmail.com>
2026-08-07 21:40:32 +0530

    Updating app.py

--- app.py
+++ app.py
@@ -3,32 +3,44 @@ import joblib
 import pandas as pd
 
 app = Flask(__name__)
+model = joblib.load("models/linear_regression_salary_model.pkl")
+preprocessor = joblib.load("models/preprocessor.pkl")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", prediction_text="")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     job_title = request.form["job_title"]
-    experience_years = request.form["experience_years"]
+    experience_years = int(request.form["experience_years"])
     education_level = request.form["education_level"]
-    skills_count = request.form["skills_count"]
+    skills_count = int(request.form["skills_count"])
     industry = request.form["industry"]
     company_size = request.form["company_size"]
     location = request.form["location"]
     remote_work = request.form["remote_work"]
-    certifications = request.form["certifications"]
-    print(job_title)
-    print(experience_years)
-    print(education_level)
-    print(skills_count)
-    print(industry)
-    print(company_size)
-    print(location)
-    print(remote_work)
-    print(certifications)
-    return "Data Received Successfully!"
+    certifications = int(request.form["certifications"])
+
+    input_data = pd.DataFrame({
+        "job_title":[job_title],
+        "experience_years":[experience_years],
+        "education_level":[education_level],
+        "skills_count":[skills_count],
+        "industry":[industry],
+        "company_size":[company_size],
+        "location":[location],
+        "remote_work":[remote_work],
+        "certifications":[certifications]
+        })
+
+    prepared_data = preprocessor.transform(input_data)
+    prediction = model.predict(prepared_data)
+    predicted_salary = round(prediction[0],2)
+    return render_template(
+    "index.html",
+    prediction_text=f"Predicted Salary: ₹ {predicted_salary:,.2f}"
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
